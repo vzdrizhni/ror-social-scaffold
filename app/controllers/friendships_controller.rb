8d31cedf984567eefc31194c42456fcdfe897e8a
vzdrizhni <vzdrizhni@gmail.com>
2020-05-12 17:57:19 +0500

    added ability to send a friend request

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,25 @@
+class FriendshipsController < ApplicationController
+
+  def index
+    @friends = current_user.friendships.all
+  end
+
+  def create
+    @friendship = current_user.friendships.build(friend_id: params[:id], confirmed: false)
+    if @friendship.save
+      redirect_to user_path, notice: 'Friendship request sent'
+    else
+      redirect_to users_path, notice: 'Vaffanculo'
+    end
+
+  end
+
+  def destroy
+
+  end
+
+  #def friendship_params
+   # params.fetch(:friendship).permit(:user_id, :friend_id, :confirmed)
+  #end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @pending_request = current_user.friend_requests
   end
+
+  #def pending_friendship(user)
+    #if current_user.pending_friends
+
+    #end
+  #end
 end
