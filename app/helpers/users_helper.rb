d28a46e12450e074ab2d89cd5471cd238af673a2
vzdrizhni <vzdrizhni@gmail.com>
2020-05-13 20:46:29 +0500

    added pending request status for current user friend request

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,8 @@
 module UsersHelper
   def confirm_button(friend)
     if current_user.friend_requests
-      link_to 'Accept', hui_user_path(friend), method: :put, class: 'profile-link'
-    else
-      link_to 'Unfriend', unfriend_user_path(friend), method: :delete, class: 'profile-link'
+      (link_to 'Accept', hui_user_path(friend), method: :put, class: 'profile-link') <<
+      (link_to 'Unfriend', unfriend_user_path(friend), method: :delete, class: 'profile-link')
     end
   end
 end
