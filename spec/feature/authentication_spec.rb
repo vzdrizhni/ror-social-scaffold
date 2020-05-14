50af07655ccd613f53aaf01325e581e24ac0906b
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-05-14 17:52:21 +0200

    refactor signin and signup into one file

--- spec/feature/authentication_spec.rb
+++ spec/feature/authentication_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
+require 'capybara/rails'
 
-RSpec.describe 'Log in', type: :feature do
+feature 'The signup process:' do
+  scenario 'user can signup' do
+    visit new_user_registration_path
+
+    fill_in 'user[name]', with: 'huihui'
+    fill_in 'user[email]', with: 'huihui@huihui.com'
+    fill_in 'user[password]', with: 'caposcapos'
+    fill_in 'user[password_confirmation]', with: 'caposcapos'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content('huihui')
+  end
+end
+
+feature 'The signin process:' do
   before(:each) do
     @user = User.new(name: 'huihui',
                      email: 'huihui@huihui.com',
