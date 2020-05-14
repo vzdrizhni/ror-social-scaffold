584f349bfe406ae33092dc37779712a28cb759b6
vzdrizhni <vzdrizhni@gmail.com>
2020-05-14 14:46:49 +0500

    user show page has add friend function

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,20 @@
 module UsersHelper
   def confirm_button(friend)
     if current_user.friend_requests
-      (link_to 'Accept', hui_user_path(friend), method: :put, class: 'profile-link') <<
-      (link_to 'Unfriend', unfriend_user_path(friend), method: :delete, class: 'profile-link')
+      (link_to 'Accept', hui_user_path(friend), method: :put, class: 'btn btn-secondary profile-link') <<
+      (link_to 'Reject', unfriend_user_path(friend), method: :delete, class: 'btn btn-secondary profile-link')
+    end
+  end
+
+  def friendship_button_show(friend)
+    if current_user != @user
+      if friend.friend_requests.any?(current_user)
+        'Pending request'
+      elsif !current_user.friend?(friend)
+        link_to 'Add friend', request_friendship_user_path(friend.id), method: :post, class: 'profile-link'
+      else
+        link_to 'Unfriend', unfriend_user_path(friend), method: :delete, class: 'profile-link'
+      end
     end
   end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,13 +4,13 @@ class Friendship < ApplicationRecord
 
   validates_presence_of :user_id, :friend_id
   validates_uniqueness_of :user, scope: :friend
-  # validate :duplicate_check
+  validate :disallow_self_friendship
 
-  # def disallow_self_friendship
-  #   if user_id == friend_id
-  #     errors.add(:friend_id, "Can't friend yourself")
-  #   end
-  # end
+   def disallow_self_friendship
+     if user_id == friend_id
+      errors.add(:friend_id, "Can't friend yourself")
+     end
+   end
 
   # def duplicate_check
   #   if Friendship.where(user_id: friend_id, friend_id: user_id).exists? && Friendship.where(user_id: user_id, friend_id: friend_id).exists?
