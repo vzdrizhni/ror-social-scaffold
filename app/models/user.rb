afc079ea59eab309fcc6782c693dc2041033a5ff
Alexandru <rammazzoti2000@gmail.com>
2020-05-18 20:18:18 +0200

    rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,12 +22,10 @@ class User < ActiveRecord::Base
     friends_array.compact
   end
 
-  # Users who have yet to confirme friend requests
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
-  # Users who have requested to be friends
   def friend_requests
     inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
