ed24530fd257c9690896c50ec143ea2d925c4560
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-05-19 08:13:40 +0200

    stickler funny behaviour won't enable

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,7 +6,7 @@ class Friendship < ApplicationRecord
   has_many :inverse_friends, through: :friendships, source: :user
 
   has_many :pending_friendships, through: :friendships, source: :friend
-  has_many :pending_friends, through: :friendships, source: :user
+  has_m
 
   validates_presence_of :user_id, :friend_id
   validates_uniqueness_of :user_id, scope: :friend_id
